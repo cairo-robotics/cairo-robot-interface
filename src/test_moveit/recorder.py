31e391a96f18fbae9e21a1c5bba2b9edff1532ee
cmuell89 <cmuell89@gmail.com>
2018-01-24 14:45:31 -0700

    In progress refactoring

--- src/test_moveit/recorder.py
+++ src/test_moveit/recorder.py
@@ -1,56 +1,6 @@
 import rospy
 import json
-import intera_interface
-
-
-class ConstraintState(object):
-
-    def __init__(self, constraints):
-        self._navigator = intera_interface.Navigator()
-        self.constraints = constraints
-
-    def get_state_dict(self):
-        state = {}
-        for constraint in self.constraints:
-            button_state = self._navigator.get_button_state(constraint["button"])
-            state[constraint["name"]] = 1 if button_state != 0 else 0
-        return state
-
-
-class RobotState(object):
-
-    def __init__(self, side="right"):
-        self._limb = intera_interface.Limb(side)
-        self._cuff = intera_interface.Cuff(side)
-        self._navigator = intera_interface.Navigator()
-        try:
-            self._gripper = intera_interface.Gripper(side)
-            rospy.loginfo("Electric gripper detected.")
-            if self._gripper.has_error():
-                rospy.loginfo("Gripper error...rebooting.")
-                self._gripper.reboot()
-            if not self._gripper.is_calibrated():
-                rospy.loginfo("Calibrating gripper.")
-                self._gripper.calibrate()
-        except Exception as e:
-            self._gripper = None
-            rospy.loginfo("No electric gripper detected.")
-
-    def get_state_dict(self):
-        state = {}
-        joints_right = self._limb.joint_names()
-        pose_right = self._limb.endpoint_pose()
-        state['PoseX'] = pose_right['position'].x
-        state['PoseY'] = pose_right['position'].y
-        state['PoseZ'] = pose_right['position'].z
-        state['OrienX'] = pose_right['orientation'].x
-        state['OrienY'] = pose_right['orientation'].y
-        state['OrienZ'] = pose_right['orientation'].z
-        state['OrienW'] = pose_right['orientation'].w
-        state['gripper'] = self._gripper.get_position()
-        for j in joints_right:
-            state[j] = self._limb.joint_angle(j)
-        return state
+from lfd_environment.interfaces import SawyerRobot, ConstraintInterface
 
 
 class Recorder(object):
@@ -74,8 +24,8 @@ class Recorder(object):
                     "button": "right_button_square"
                 }
             ]
-        self.constraint_state = ConstraintState(self.constraints)
-        self.robot_state = RobotState(side=side)
+        self.constraint_state = ConstraintInterface(self.constraints)
+        self.robot_state = SawyerRobot(side=side)
 
     def _time_stamp(self):
         return rospy.get_time() - self._start_time
