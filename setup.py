a7f45d68f497a57d82ed0876b06ca339269eaa7f
Carl Mueller <cmuell89@gmail.com>
2018-01-31 21:36:20 -0700

    removed lfd_environment package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['lfd_environment', 'test_moveit']
+d['packages'] = ['test_moveit']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
