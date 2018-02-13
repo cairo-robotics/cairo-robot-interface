374cdef28a6ee4a81ca3db6a6f2ea93e5a5add9a
Jeff <jeve0658@colorado.edu>
2018-02-12 21:11:12 -0700

    ugly but WORKS!

--- src/forward_kinematic_server.cpp
+++ src/forward_kinematic_server.cpp
@@ -47,39 +47,76 @@
 
 
 
-class sawyer_fk{
+class SawyerFK{
   //robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
 
 
 public:
   robot_model::RobotModelPtr kinematic_model;
-  sawyer_fk();
+  robot_state::RobotStatePtr kinematic_state2;
+  std::vector<double> joint_values;
+  robot_state::JointModelGroup *joint_model_group;
+  SawyerFK();
   void callback(const std_msgs::Float32MultiArray::ConstPtr&);
 
 };
 
-sawyer_fk::sawyer_fk(){
+SawyerFK::SawyerFK(){
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
   kinematic_model = robot_model_loader.getModel();
-  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
   robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
-
+  kinematic_state2 = kinematic_state;
+  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
+  joint_model_group = kinematic_model->getJointModelGroup("right_arm");
 }
 
 
-void sawyer_fk::callback(const std_msgs::Float32MultiArray::ConstPtr& msg){
-  ROS_INFO("I heard a joint message");
+void SawyerFK::callback(const std_msgs::Float32MultiArray::ConstPtr& msg){
+
+  std::size_t count = kinematic_state2->getVariableCount();
+  ROS_INFO("state count: %zu", count);
+  const std::vector<std::string> &joint_names = kinematic_state2->getVariableNames();
+  for(std::size_t i = 0; i < joint_names.size(); i++){
+    ROS_INFO("joint names: %s", joint_names[i].c_str());
+  }
+
+  for(std::size_t i = 0; i < msg->data.size(); i++){
+    ROS_INFO("send values: %f", msg->data[i]);
+  }
+  std::vector<double> position;
+  position.push_back(msg->data[0]);
+  position.push_back(.99);
+  for(std::size_t i = 1; i < msg->data.size(); i++){
+    position.push_back(msg->data[i]);
+  }
+  for(std::size_t i = 0; i < position.size(); i++){
+    ROS_INFO("made position: %f", position[i]);
+  }
+  kinematic_state2->setVariablePositions(position);
+  const Eigen::Affine3d &end_effector_state = kinematic_state2->getGlobalLinkTransform("right_gripper_base");
+
+  ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
+  ROS_INFO_STREAM("Rotation: " << end_effector_state.rotation());
+
+
+
+  //std::size_t count =  kinematic_model->getJointModelCount();
+  //kinematic_model->copyJointGroupPositions(joint_model_group, joint_values);
+  //for( std::size_t i = 0; i < joint_values.size(); i++){
+  //  ROS_INFO("joint value: %zu", joint_values[i]);
+  //}
+
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "right_arm_kinematics");
   ros::NodeHandle n;
-  //ros::Subscriber sub = n.subscribe("fk_joints", 10, callback );
-  //ros::AsyncSpinner spinner(1);
-  //spinner.start();
-  sawyer_fk sawyer_fk();
 
+  SawyerFK sawyer = SawyerFK();
+  ros::Subscriber sub = n.subscribe("fk_joints", 10, &SawyerFK::callback, &sawyer);
+
+  ros::spin();
 
 
 /*
@@ -130,7 +167,6 @@ int main(int argc, char **argv)
   ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
   ROS_INFO_STREAM("Rotation: " << end_effector_state.rotation());
   */
-  ros::spin();
   //ros::shutdown();
   return 0;
 }
