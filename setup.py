c6ad0e74fd93ed1cd4aceadbc6ee680c157e9a73
Carl Mueller <cmuell89@gmail.com>
2018-02-21 20:29:30 -0700

    new package name and renaming of modules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['sawyer_interface', "forward_kinematics"]
+d['packages'] = ['sawyer_interface', "forward_kinematics", "kinematics_interface"]
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
